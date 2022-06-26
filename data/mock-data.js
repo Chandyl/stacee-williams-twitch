70850184219ed966ea71af874bd6ef18fab8750f
Stacee <swilliams134@collin.edu>
2022-06-26 18:03:01 -0500

    first commit

---
+++ data/mock-data.js
@@ -0,0 +1,105 @@
+export const rec_channels = [
+	{
+		id: 1,
+		avatar: "https://robohash.org/aliasomnisasperiores.png?size=50x50&set=set1",
+		username: "fwarkup0",
+		game_name: "Sonair",
+		rank: 38,
+	},
+	{
+		id: 2,
+		avatar: "https://robohash.org/inatqueculpa.png?size=50x50&set=set1",
+		username: "jbrabbins1",
+		game_name: "Zoolab",
+		rank: 78,
+	},
+	{
+		id: 3,
+		avatar: "https://robohash.org/placeatearumeos.png?size=50x50&set=set1",
+		username: "lamey2",
+		game_name: "Otcom",
+		rank: 47,
+	},
+	{
+		id: 4,
+		avatar: "https://robohash.org/accusantiumametodit.png?size=50x50&set=set1",
+		username: "lbrowell3",
+		game_name: "Mat Lam Tam",
+		rank: 96,
+	},
+	{
+		id: 5,
+		avatar: "https://robohash.org/autquisoluta.png?size=50x50&set=set1",
+		username: "ccotterel4",
+		game_name: "Cardguard",
+		rank: 86,
+	},
+	{
+		id: 6,
+		avatar: "https://robohash.org/odionihilnumquam.png?size=50x50&set=set1",
+		username: "kdederich5",
+		game_name: "Redhold",
+		rank: 27,
+	},
+	{
+		id: 7,
+		avatar: "https://robohash.org/exrepellatut.png?size=50x50&set=set1",
+		username: "wdebischop6",
+		game_name: "Domainer",
+		rank: 1,
+	},
+	{
+		id: 8,
+		avatar:
+			"https://robohash.org/minuslaboriosamnumquam.png?size=50x50&set=set1",
+		username: "trisman7",
+		game_name: "Tresom",
+		rank: 12,
+	},
+	{
+		id: 9,
+		avatar: "https://robohash.org/totamnesciuntlaborum.png?size=50x50&set=set1",
+		username: "jhightown8",
+		game_name: "Stringtough",
+		rank: 3,
+	},
+	{
+		id: 10,
+		avatar: "https://robohash.org/laborumautsimilique.png?size=50x50&set=set1",
+		username: "kgibbe9",
+		game_name: "Konklux",
+		rank: 22,
+	},
+];
+
+export const top_channels = [
+	{
+		id: 1,
+		avatar: "https://robohash.org/repellendusutaut.png?size=50x50&set=set1",
+		username: "dlegrand0",
+		game_name: "Lotstring",
+		rank: 37,
+	},
+	{
+		id: 2,
+		avatar: "https://robohash.org/doloreiusrepudiandae.png?size=50x50&set=set1",
+		username: "acharnick1",
+		game_name: "Redhold",
+		rank: 49,
+	},
+	{
+		id: 3,
+		avatar: "https://robohash.org/autametdolor.png?size=50x50&set=set1",
+		username: "talwood2",
+		game_name: "Transcof",
+		rank: 80,
+	},
+	{
+		id: 4,
+		avatar:
+			"https://robohash.org/quaealiquidvoluptates.png?size=50x50&set=set1",
+		username: "rmcclymond3",
+		game_name: "Namfix",
+		rank: 16,
+	},
+];
